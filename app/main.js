d35b09dc5311166fb93a47f256995893176d87a4
Tolu Oluwagbemi <tulburg@yahoo.com>
2024-06-11 19:49:35 +0200

    Update to apple M-series chips

--- app/main.js
+++ app/main.js
@@ -1,18 +1,19 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
-const {is} = require('electron-util');
-const path = require('path');
-const TrayGenerator = require('./tray-generator');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { is } = require("electron-util");
+const path = require("path");
+const TrayGenerator = require("./tray-generator");
 
 let mainWindow = null;
-const Store = require('electron-store');
+const Store = require("electron-store");
 const schema = {
-  launchAtStart: true
-}
-const store = new Store(schema); null;
+  launchAtStart: true,
+};
+const store = new Store(schema);
+null;
 
 const createMainWindow = () => {
   mainWindow = new BrowserWindow({
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     width: 420,
     height: 600,
     show: false,
@@ -20,39 +21,37 @@ const createMainWindow = () => {
     fullscreenable: false,
     resizable: false,
     webPreferences: {
-      devTools: is.development,
+      devTools: true,
       nodeIntegration: true,
-      contextIsolation: false
-    }
+      contextIsolation: false,
+    },
   });
 
-  mainWindow.on('blur', () => {
+  mainWindow.on("blur", () => {
     mainWindow.hide();
     app.dock.hide();
   });
   if (is.development) {
-    mainWindow.webContents.openDevTools({mode: 'detach'});
-    // mainWindow.loadURL('http://localhost:8080');
-    mainWindow.loadURL(`file://${path.join(__dirname, '/public/index.html')}`);
+    mainWindow.webContents.openDevTools({ mode: "detach" });
+    mainWindow.loadURL("http://localhost:8086");
   } else {
     mainWindow.webContents.openDevTools();
-    mainWindow.loadURL(`file://${path.join(__dirname, '/public/index.html')}`);
+    mainWindow.loadURL(`file://${path.join(__dirname, "/public/index.html")}`);
   }
 };
 
-app.on('ready', () => {
+app.on("ready", () => {
   createMainWindow();
   const Tray = new TrayGenerator(mainWindow, store);
   Tray.createTray();
 
-  ipcMain.on('update-title', (_, data) => {
+  ipcMain.on("update-title", (_, data) => {
     Tray.setTrayTitle(data);
-  })
+  });
 });
 
 app.setLoginItemSettings({
-  openAtLogin: store.get('launchAtStart'),
+  openAtLogin: store.get("launchAtStart"),
 });
 
 app.dock.hide();
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
 /* eslint-disable */
-var path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, 'node_modules/@javascriptui/core/dist/instance'),
-  target: 'electron-renderer',
-  node: {global: true},
+  mode: process.env.mode,
+  entry: path.resolve(
+    __dirname,
+    "node_modules/@javascriptui/core/dist/instance"
+  ),
+  target: "electron-renderer",
+  node: { global: true },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
+    port: 8086,
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      filename: path.resolve(__dirname, 'app/public/index.html'),
-      template: path.resolve(__dirname, 'src/index.html'),
+      filename: path.resolve(__dirname, "app/public/index.html"),
+      template: path.resolve(__dirname, "src/index.html"),
     }),
   ],
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, 'src'),
+      "@src": path.resolve(__dirname, "src"),
     },
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   output: {
-    path: path.resolve(__dirname, 'app/public'),
-    filename: 'app.js',
-    library: 'js-native',
-    libraryTarget: 'umd',
-    publicPath: '/'
+    path: path.resolve(__dirname, "app/public"),
+    filename: "app.js",
+    library: "js-native",
+    libraryTarget: "umd",
+    publicPath: process.env.mode === "production" ? "" : "/",
   },
   module: {
     rules: [
-      {test: /\.tsx?$/, loader: 'ts-loader'},
-      {test: /\.svg$/, type: 'asset/resource'},
-      {test: /\.(png|jpg|gif)$/, type: 'asset/resource'},
-      {test: /\.(woff(2)?|ttf|eot|svg)?$/, type: 'asset/resource'},
-      {test: /\.css$/, use: ['style-loader', 'css-loader']}
-    ]
-  }
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.svg$/, type: "asset/resource" },
+      { test: /\.(png|jpg|gif)$/, type: "asset/resource" },
+      { test: /\.(woff(2)?|ttf|eot|svg)?$/, type: "asset/resource" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
 };
